eb7f1117818fd887dd0ae8db3518be3222708101
sc6l6d3v <hk-vndr@iscs-i.com>
2021-08-25 21:59:20 -0400

    Fix adult search.

--- src/main/scala/com/iscs/releaseScraper/domains/ImdbQuery.scala
+++ src/main/scala/com/iscs/releaseScraper/domains/ImdbQuery.scala
@@ -3,7 +3,7 @@ package com.iscs.releaseScraper.domains
 import cats.effect.{Concurrent, ConcurrentEffect, Sync}
 import cats.implicits._
 import com.iscs.releaseScraper.model.Requests.ReqParams
-import com.iscs.releaseScraper.util.DbClient
+import com.iscs.releaseScraper.util.{DbClient, asInt}
 import com.typesafe.scalalogging.Logger
 import fs2.{Pipe, Stream}
 import io.circe._
@@ -14,8 +14,9 @@ import org.http4s.circe._
 import org.http4s.{EntityDecoder, EntityEncoder}
 import org.mongodb.scala.bson.{BsonDocument, BsonNumber}
 import org.mongodb.scala.bson.collection.immutable.Document
-import org.mongodb.scala.model.Filters.{and, exists, gte, regex, text, elemMatch => elemMatchFilter, eq => mdbeq, ne => mdne}
+import org.mongodb.scala.model.Filters.{and, exists, gte, regex, text, eq => mdbeq, ne => mdne}
 import org.mongodb.scala.model._
+import scala.language.implicitConversions
 
 trait ImdbQuery[F[_]] {
   def getByTitle(title: String, rating: Double, params: ReqParams): Stream[F,Json]
@@ -59,8 +60,8 @@ object ImdbQuery {
       val primaryName = "primaryName"
       val lastName = "lastName"
       val firstName = "firstName"
-      val primaryProfession = "primaryProfession"
-      val primaryProfessionList = "primaryProfessionList"
+/*      val primaryProfession = "primaryProfession"
+      val primaryProfessionList = "primaryProfessionList"*/
       val knownForTitles = "knownForTitles"
       val knownForTitlesList = "knownForTitlesList"
       val titleCollection = "title_basics_ratings"
@@ -68,17 +69,17 @@ object ImdbQuery {
       val birthYear = "birthYear"
       val deathYear = "deathYear"
       val matchedTitles = "matchedTitles"
-      val startYear = "startYear"
+//      val startYear = "startYear"
       val genres = "genres"
-      val genresList = "genresList"
+//      val genresList = "genresList"
       val matchedTitles_averageRating = "matchedTitles.averageRating"
       val matchedTitles_genres = "matchedTitles.genres"
       val matchedTitles_genresList = "matchedTitles.genresList"
       val matchedTitles_isAdult = "matchedTitles.isAdult"
       val matchedTitles_startYear = "matchedTitles.startYear"
       val matchedTitles_titleType = "matchedTitles.titleType"
-      val titleType = "titleType"
-      val isAdult = "isAdult"
+//      val titleType = "titleType"
+//      val isAdult = "isAdult"
       private val titleFx = dbClient.fxMap(titleCollection)
       private val nameFx = dbClient.fxMap(nameCollection)
 
@@ -104,28 +105,17 @@ object ImdbQuery {
 
       def getTitleModelFilters(title: String): F[Bson] = for {
         parts <- Concurrent[F].delay(title.split(" ").toList)
-        regexList <- Concurrent[F].delay(parts.map(part =>
-          regex(primaryTitle, s"/$part/i")
-        ))
-        regex <- condenseSingleLists(regexList)
         textBson <- Concurrent[F].delay(text(title))
-        bson <- Concurrent[F].delay(and(textBson, regex))
       } yield textBson
+
+      implicit def convertBooleanToInt(b: Boolean): asInt = new asInt(b)
 
       def getParamList(params: ReqParams): F[List[Bson]] = Concurrent[F].delay(
         List(
-          params.year.map(yr => mdbeq(startYear, yr)),
-          params.genre.map(genre => mdbeq(genresList, genre)),
-          params.titleType.map(tt => mdbeq(titleType, tt)),
-          params.isAdult.map(isadult => mdbeq(isAdult, isadult))
-        ).flatten)
-
-      def getAggregationParamList(params: ReqParams): F[List[Bson]] = Concurrent[F].delay(
-        List(
-          params.year.map(yr => nameFx.getCompareFilter("eq", s"$$$matchedTitles_startYear", yr)),
-          params.genre.map(genre => elemMatchFilter(genresList, nameFx.getCompareFilter("eq", s"$$$matchedTitles_genresList", genre))),
-          params.titleType.map(tt => nameFx.getCompareFilter("eq", s"$$$matchedTitles_titleType", tt)),
-          params.isAdult.map(isadult => nameFx.getCompareFilter("eq", s"$$$matchedTitles_isAdult", isadult))
+          params.year.map(yr => mdbeq(matchedTitles_startYear, yr)),
+          params.genre.map(genre => mdbeq(matchedTitles_genresList, genre)),
+          params.titleType.map(tt => mdbeq(matchedTitles_titleType, tt)),
+          params.isAdult.map(isadult => mdbeq(matchedTitles_isAdult, isadult.toInt))
         ).flatten)
 
       def getParamModelFilters(params: ReqParams): F[Bson] = for {
@@ -159,34 +149,29 @@ object ImdbQuery {
        *       'foreignField': '_id',
        *       'as': 'matchedTitles'
        * }, {
-       *       $match: {
-       *         "matchedTitles": {
-       *            $elemMatch: {
-       *              "startYear": 1986
-       *             }
-       *         }
-       *       }
-       * }, {
-       *    $project: { primaryProfession: 0, deathYear: 0, knownForTitles: 0, primaryProfessionList: 0, knownForTitlesList: 0,
-       *        "matchedTitles.genres": 0  }
-       * }, {
        *    $project: {
-       *       "primaryName": 1, birthYear: 1,
-       *       "matchedTitles": {
-       *          $filter: {
-       *             "input": "$matchedTitles",
-       *             "as": "matchedTitles",
-       *             "cond": {
-       *                "$and": [
-       *                    { $gte: [ "$$matchedTitles.averageRating", 7.0 ] },
-       *                    { $eq: [ "$$matchedTitles.startYear": 1986 ] },
-       *                    { $eq: [ "$$matchedTitles.genresList": "Drama" ] },
-       *                    { $eq: [ "$$matchedTitles.titleType": "movie" ] },
-       *                    { $eq: [ "$$matchedTitles.isAdult": 1 ] },
-       *                ]
-       *             }
-       *          }
-       *       }
+       *        "matchedTitles.genres": 0  }
+       *    }
+       * }, {
+       *    $unwind: '$matchedTitles'
+       * }, {
+       *    $match: { $and: [
+       *                    { "$$matchedTitles.averageRating", 7.0 },
+       *                    { "$$matchedTitles.startYear": 1986 },
+       *                    { "$$matchedTitles.genresList": "Drama" },
+       *                    { "$$matchedTitles.titleType": "movie" },
+       *                    { "$$matchedTitles.isAdult": 1 },
+       *              ]
+       *            }
+       * }, {
+       *    $group: {
+       *       _id: "$_id",
+       *       primaryName: { $first: "$primaryName" },
+       *       firstName: { $first: "$firstName" },
+       *       lastName: { $first: "$firstName" },
+       *       birthYear: { $first: "$birthYear" },
+       *       deathYear: { $first: "$deathYear" },
+       *       matchedTitles: { $push: "$matchedTitles" },
        *    }
        * }
        *
@@ -196,9 +181,6 @@ object ImdbQuery {
        * @return
        */
       override def getByName(name: String, rating: Double, params: ReqParams): Stream[F, Json] = for {
-        paramsList <- Stream.eval(getParamList(params))
-        ratingBson <- Stream.eval(Concurrent[F].delay(gte("averageRating", rating)))
-        bsonCondensedList <- Stream.eval(condenseSingleLists(paramsList :+ ratingBson))
         matchTitleWithName <- Stream.eval(Concurrent[F].delay(
           and(
             exists(knownForTitles, exists = true),
@@ -209,55 +191,53 @@ object ImdbQuery {
         titleMatchFilter <- Stream.eval(Concurrent[F].delay(
           Aggregates.filter(matchTitleWithName)
         ))
-        matchLookupsFilter <- Stream.eval(Concurrent[F].delay(
-          Aggregates.filter(elemMatchFilter(matchedTitles, bsonCondensedList))
-        ))
+
         lookupFilter <- Stream.eval(Concurrent[F].delay(
           Aggregates.lookup(titleCollection,
             knownForTitlesList,
             "_id",
             matchedTitles)
         ))
-        initialProjectionsList <- Stream.eval(Concurrent[F].delay(
+
+        paramsList <- Stream.eval(getParamList(params))
+        ratingBson <- Stream.eval(Concurrent[F].delay(gte(matchedTitles_averageRating, rating)))
+        bsonCondensedList <- Stream.eval(condenseSingleLists(paramsList :+ ratingBson))
+        matchLookupsFilter <- Stream.eval(Concurrent[F].delay(
+          Aggregates.filter(and(bsonCondensedList))
+        ))
+
+        projectionsList <- Stream.eval(Concurrent[F].delay(
           nameFx.getProjectionFields(Map(
-            primaryProfession -> false,
-            deathYear -> false,
-            knownForTitles -> false,
-            primaryProfessionList -> false,
-            knownForTitlesList -> false,
-            matchedTitles_genres -> false,
+            matchedTitles_genres -> false
           ))
         ))
-        initialProjectFilter <- Stream.eval(Concurrent[F].delay(
-          Aggregates.project(nameFx.getProjections(initialProjectionsList))
+        projectionFilter <- Stream.eval(Concurrent[F].delay(
+          Aggregates.project(nameFx.getProjections(projectionsList))
         ))
-        finalProjectionsList <- Stream.eval(Concurrent[F].delay(
-          nameFx.getProjectionFields(Map(
-            primaryName -> true,
-            birthYear -> true
-          ))
+
+        unwindFilter <- Stream.eval(Concurrent[F].delay(
+          Aggregates.unwind(s"$$$matchedTitles")
         ))
-        aggParamsList <- Stream.eval(getAggregationParamList(params))
-        condCondensedList <- Stream.eval(condenseSingleLists(
-          aggParamsList :+
-          nameFx.getCompareFilter("gte", s"$$$matchedTitles_averageRating", rating)) // TODO
-        )
-        condFilter <- Stream.eval(Concurrent[F].delay(
-          nameFx.getCondFilter("matchedTitles", "matchedTitles", condCondensedList)
+
+        groupFilter <- Stream.eval(Concurrent[F].delay(
+          Aggregates.group("$_id",
+            Accumulators.first(primaryName, s"$$$primaryName"),
+            Accumulators.first(firstName, s"$$$firstName"),
+            Accumulators.first(lastName, s"$$$lastName"),
+            Accumulators.first(birthYear, s"$$$birthYear"),
+            Accumulators.first(deathYear, s"$$$deathYear"),
+            Accumulators.push(matchedTitles, s"$$$matchedTitles")
+          )
         ))
-        combinedProjections <- Stream.eval(Concurrent[F].delay(
-          finalProjectionsList :+ condFilter
-        ))
-        finalProjectFilter <- Stream.eval(Concurrent[F].delay(
-          Aggregates.project(nameFx.getProjections(combinedProjections))
-        ))
+
         dbList <- Stream.eval(nameFx.aggregate(
           Seq(
             titleMatchFilter,
             lookupFilter,
+            projectionFilter,
+            unwindFilter,
             matchLookupsFilter,
-            initialProjectFilter,
-            finalProjectFilter
+            groupFilter
           )
         )
           .through(docToJson)
---
+++ src/main/scala/com/iscs/releaseScraper/util/asInt.scala
@@ -0,0 +1,5 @@
+package com.iscs.releaseScraper.util
+
+class asInt(b: Boolean) {
+  def toInt = if(b) 1 else 0
+}
