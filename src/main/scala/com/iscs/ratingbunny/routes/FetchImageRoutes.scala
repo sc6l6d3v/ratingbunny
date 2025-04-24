0767e13f21f8cf14ebb0ef2c80649404749d531f
sc6l6d3v <hk-vndr@iscs-i.com>
2025-04-23 23:21:44 -0400

    Add Cache-Control to api/.../image endpoint.

--- src/main/scala/com/iscs/ratingbunny/routes/FetchImageRoutes.scala
+++ src/main/scala/com/iscs/ratingbunny/routes/FetchImageRoutes.scala
@@ -5,9 +5,13 @@ import cats.implicits.*
 import com.iscs.ratingbunny.domains.FetchImage
 import com.typesafe.scalalogging.Logger
 import org.http4s.*
+import org.http4s.CacheDirective
 import org.http4s.MediaType.image.*
 import org.http4s.dsl.Http4sDsl
-import org.http4s.headers.`Content-Type`
+import org.http4s.headers.{`Cache-Control`, `Content-Type`}
+
+import scala.concurrent.duration.*
+import scala.language.postfixOps
 
 object FetchImageRoutes {
   private val L          = Logger[this.type]
@@ -19,8 +23,17 @@ object FetchImageRoutes {
     val imgSvc = HttpRoutes
       .of[F] { case _ @GET -> Root / "api" / `apiVersion` / "image" / imdbId =>
         for {
-          _    <- Sync[F].delay(L.info(s""""request" image=$imdbId"""))
+          _ <- Sync[F].delay(L.info(s""""request" image=$imdbId"""))
           resp <- Ok(R.getImage(imdbId))
+            .map(
+              _.putHeaders(
+                `Content-Type`(MediaType.image.jpeg),
+                `Cache-Control`(
+                  CacheDirective.`public`,
+                  CacheDirective.`max-age`(86400 seconds) // 1 day in seconds
+                )
+              )
+            )
         } yield resp
       }
       .map(_.withContentType(`Content-Type`(`jpeg`)))
