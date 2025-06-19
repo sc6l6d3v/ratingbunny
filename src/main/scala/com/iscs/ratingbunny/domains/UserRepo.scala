0d80a6d310b8fb372d912aa15c5c32139a27be9c
sc6l6d3v <hk-vndr@iscs-i.com>
2025-06-19 17:16:04 -0400

    Update `findByUserId` to query by `userid` instead of `_id` in `UserRepo`

--- src/main/scala/com/iscs/ratingbunny/domains/UserRepo.scala
+++ src/main/scala/com/iscs/ratingbunny/domains/UserRepo.scala
@@ -19,5 +19,5 @@ trait UserRepo[F[_]] {
 class UserRepoImpl[F[_]: Async](collection: MongoCollection[F, UserDoc]) extends UserRepo[F] with QuerySetup {
   override def findByEmail(email: String): F[Option[UserDoc]] = collection.find(feq("email", email)).first
   override def insert(u: UserDoc): F[Unit]                    = collection.insertOne(u).void
-  override def findByUserId(id: String): F[Option[UserDoc]]   = collection.find(feq("_id", id)).first
+  override def findByUserId(uid: String): F[Option[UserDoc]]  = collection.find(feq("userid", uid)).first
 }
