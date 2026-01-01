4c98666951d719cf90c29e514b6f5c33fc4c101c
White Rose <rkatz1997@hotmail.com>
2026-01-01 14:40:35 -0500

    Restrict history routes to pro verified users

---
+++ src/main/scala/com/iscs/ratingbunny/routes/VerifiedUserRoutes.scala
@@ -0,0 +1,25 @@
+package com.iscs.ratingbunny.routes
+
+import cats.effect.Async
+import com.iscs.ratingbunny.config.TrialConfig
+import com.iscs.ratingbunny.domains.UserRepo
+import org.http4s.Response
+import org.http4s.dsl.Http4sDsl
+
+trait VerifiedUserRoutes:
+
+  protected def ensureVerified[F[_]: Async](
+      uid: String,
+      trialConfig: TrialConfig,
+      userRepo: UserRepo[F]
+  )(
+      body: => F[Response[F]]
+  ): F[Response[F]] =
+    val dsl = Http4sDsl[F]; import dsl.*
+
+    if trialConfig.enabled then body
+    else
+      userRepo.findByUserId(uid).flatMap {
+        case Some(u) if u.emailVerified => body
+        case _                          => Forbidden()
+      }
